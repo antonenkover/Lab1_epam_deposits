630e2940764c5d357eb3ec5002fd6ec862ba4795
antonenkover <antonenkover@gmail.com>
2019-10-01 18:52:16 +0300

    removed unused bank classes

--- src/main/java/laba1/com/AlphaBank.java
+++
@@ -1,7 +0,0 @@
-package laba1.com;
-
-public class AlphaBank extends Bank {
-    public AlphaBank(String name, int numOfDeposits) {
-        super(name, numOfDeposits);
-    }
-}
--- src/main/java/laba1/com/PrivatBank.java
+++
@@ -1,7 +0,0 @@
-package laba1.com;
-
-public class PrivatBank extends Bank {
-    public PrivatBank(String name, int numOfDeposits) {
-        super(name, numOfDeposits);
-    }
-}
