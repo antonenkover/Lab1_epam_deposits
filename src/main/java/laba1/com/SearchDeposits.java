35c39a25bec245b2e9dbb9bb4ed7312254664f72
antonenkover <antonenkover@gmail.com>
2019-10-07 14:42:47 +0300

    added logging to files

--- src/main/java/laba1/com/SearchDeposits.java
+++ src/main/java/laba1/com/SearchDeposits.java
@@ -1,20 +1,28 @@
 package laba1.com;
 
+import org.apache.log4j.Logger;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class SearchDeposits {
+    private static final Logger LOG = Logger.getLogger(SearchDeposits.class);
+
     public static void search(List<Deposit> deposits, Deposit.Fields searchType, int from, int to) {
+        LOG.info("Deposits list for search: " + deposits.toString());
         List<Deposit> searchedList = new ArrayList<>();
         switch (searchType) {
             case Percentage:
                 searchByPercentage(deposits, from, to, searchedList);
+                LOG.info("Chosen search parameter: " + searchType);
                 break;
             case MinimumSum:
                 searchByMinumumSum(deposits, from, to, searchedList);
+                LOG.info("Chosen search parameter: " + searchType);
                 break;
             case DepositDuration:
                 searchByDepositDuration(deposits, from, to, searchedList);
+                LOG.info("Chosen search parameter: " + searchType);
                 break;
         }
     }
@@ -25,6 +33,7 @@ public class SearchDeposits {
                 searchedList.add(deposit);
         }
         searchedList.forEach(System.out::println);
+        LOG.info(String.format("Searched list by Deposit duration: from %s to %s", from, to) + searchedList.toString());
         if (searchedList.isEmpty())
             System.out.println("Deposit was not found\n");
     }
@@ -35,6 +44,7 @@ public class SearchDeposits {
                 searchedList.add(deposit);
         }
         searchedList.forEach(System.out::println);
+        LOG.info(String.format("Searched list by Minimum Sum: from %s to %s", from, to) + searchedList.toString());
         if (searchedList.isEmpty())
             System.out.println("Deposit was not found\n");
     }
@@ -45,6 +55,7 @@ public class SearchDeposits {
                 searchedList.add(deposit);
         }
         searchedList.forEach(System.out::println);
+        LOG.info(String.format("Searched list by Percentage: from %s to %s", from, to) + searchedList.toString());
         if (searchedList.isEmpty())
             System.out.println("Deposit was not found\n");
     }
--- src/main/java/laba1/com/SortDeposits.java
+++ src/main/java/laba1/com/SortDeposits.java
@@ -1,20 +1,28 @@
 package laba1.com;
 
+import org.apache.log4j.Logger;
+
 public class SortDeposits {
+
+    private static final Logger LOG = Logger.getLogger(SortDeposits.class);
 
     public static void sortDeposits(Bank bank, Deposit.Fields sortingType) {
         switch (sortingType) {
             case Percentage:
                 bank.bankdeposits.sort(Deposit::compareByPercentage);
+                LOG.info(String.format("SortDeposits of %s by %s", bank.getName(), sortingType));
                 break;
             case DepositDuration:
                 bank.bankdeposits.sort(Deposit::compareByDepositDuration);
+                LOG.info(String.format("SortDeposits of %s by %s", bank.getName(), sortingType));
                 break;
             case MinimumSum:
                 bank.bankdeposits.sort(Deposit::compareByMinimumSum);
+                LOG.info(String.format("SortDeposits of %s by %s", bank.getName(), sortingType));
                 break;
             case Cancellation:
                 bank.bankdeposits.sort(Deposit::compareByCancellationAvaliability);
+                LOG.info(String.format("SortDeposits of %s by %s", bank.getName(), sortingType));
                 break;
         }
     }
